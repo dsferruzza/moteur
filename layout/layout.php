008e3e5a622bf026623b1c9af3cd3a0b2058e8d3
David Sferruzza <david.sferruzza@gmail.com>
2012-07-08 21:43:15 +0200

    Message d'explication en haut des pages d'exemple

--- layout/layout.php
+++ layout/layout.php
@@ -7,6 +7,9 @@
 </head>
 <body>
 	<div>
+		<p>Ceci est un petit site d'exemple réalisé à la va-vite pour montrer et tester les fonctionnalités de mon moteur.<br />
+		Pour bien comprendre le fonctionnement dudit moteur, il est conseillé de lire le fichier README et de regarder le code source des pages, actions et layouts qui me servent ici d'exemple.</p>
+		<h1>Menu</h1>
 		<a href="<?php echo url() ?>">Index</a><br />
 		<a href="<?php echo url(null, 'test=azerty') ?>">Index (avec paramètre GET)</a><br />
 		<a href="<?php echo url('test1') ?>">Test1</a><br />
